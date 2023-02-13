1253ea08acbb6a3a6972b76d577eaa20e8d49dd1
Alejandra Guzman <77361246+guzmanalejandra@users.noreply.github.com>
2023-02-13 08:59:35 -0600

    cambios en ipc.c

--- ipc.c
+++ ipc.c
@@ -1,54 +1,72 @@
 #include <stdio.h>
-#include <unistd.h>
 #include <stdlib.h>
-#include <fcntl.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 #include <string.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <sys/wait.h>
 
+#define SHM_KEY 123456
+#define SHM_SIZE 1024
 
-#define SHM_NAME "SHARED_MEMORY"
-#define SHM_SIZE 4096
+int main(int argc, char *argv[]) {
+    int num, shm_id, i, pid;
+    char letra;
+    char *shm_ptr;
 
-int main(int argc, char *argv[]){
-    if(argc < 3) { 
-        printf("Not enough args passed! Please provide at least two arguments.\n"); 
-        exit(1); 
+    // Recibir el número y la letra desde la línea de comandos
+    if (argc < 3) {
+        printf("Uso: %s <numero> <letra>\n", argv[0]);
+        return 1;
+    }
+    num = atoi(argv[1]);
+    letra = argv[2][0];
+
+    // Crear o conectarse a un espacio de memoria compartida
+    shm_id = shmget(SHM_KEY, SHM_SIZE, IPC_CREAT | IPC_EXCL | S_IRUSR | S_IWUSR);
+    if (shm_id == -1) {
+        shm_id = shmget(SHM_KEY, SHM_SIZE, S_IRUSR | S_IWUSR);
+        if (shm_id == -1) {
+            perror("shmget");
+            return 1;
+        }
     }
 
-    char* n = argv[1];
-    char* x = argv[2];
-    int shm = shm_open(SHM_NAME, O_RDWR | O_CREAT, 0666);
-
-    if (shm < 0) {
-        printf("Shared memory creation failed.\n");
-        exit(1);
+    // Mapear la memoria compartida a un puntero y crear un proceso hijo
+    shm_ptr = shmat(shm_id, NULL, 0);
+    if (shm_ptr == (void *) -1) {
+        perror("shmat");
+        return 1;
+    }
+    pid = fork();
+    if (pid == -1) {
+        perror("fork");
+        return 1;
     }
 
-    if (ftruncate(shm, SHM_SIZE) == -1) {
-        printf("Failed to resize the shared memory.\n");
-        exit(1);
-    }
-
-    void * maddr = mmap(0, SHM_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm, 0);
-
-    if (maddr == MAP_FAILED) {
-        printf("Mapping to shared memory failed.\n");
-        exit(1);
-    }
-
-    strncpy(maddr, x, strlen(x));
-
-    if (fork() == 0) {
-        printf("Child: shared memory content: %s\n", (char*) maddr);
-    } else {
+    // Proceso padre
+    if (pid > 0) {
+        for (i = 0; i < SHM_SIZE; i++) {
+            if (i % num == 0) {
+                shm_ptr[i] = letra;
+                printf("Proceso padre: escribiendo %c en posición %d\n", letra, i);
+            }
+        }
         wait(NULL);
-        printf("Parent: shared memory content: %s\n", (char*) maddr);
-        shm_unlink(SHM_NAME);
+        printf("Contenido de la memoria compartida:\n");
+        for (i = 0; i < SHM_SIZE; i++) {
+            printf("%c", shm_ptr[i]);
+        }
+        printf("\n");
     }
-
+    // Proceso hijo
+    else {
+        for (i = 0; i < SHM_SIZE; i++) {
+            if (shm_ptr[i] == letra) {
+                printf("Proceso hijo: leyendo");
+            }
+        }
+    }
     return 0;
 }
-
